cb33bff6fb2ea335cfbfa71c4590482ca1bae664
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-20 17:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let tr = document.createElement('tr');
+	let td1 = document.createElement('td')
+	td1.innerHTML = 'New Cell1';
+	let td2 = document.createElement('td')
+	td2.innerHTML = 'New Cell2';
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	let table = document.getElementById('sampleTable');
+	table.insertBefore(tr, table.firstChild);
 }
