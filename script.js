13d75032457136595a3c8bf8b657ade0b57c2c86
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-21 00:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function insert_Row() {
     //Write your code here
 	let tr = document.createElement('tr');
-	// tr.innerHTML = '<tr><td>New Cell1</td><td>New Cell2</td></tr>';
-	let td1 = document.createElement('td')
-	td1.innerHtml = '';
+	let td1 = document.createElement('td');
 	td1.innerHTML = 'New Cell1';
 	let td2 = document.createElement('td')
-	td2.innerHtml = '';
 	td2.innerHTML = 'New Cell2';
 	tr.insertAdjacentElement('afterbegin', td1);
 	tr.insertAdjacentElement('beforeend', td2);
